faf33004b95876171153748827fe3e6edcaf7179
zhijunwen <1638672281@qq.com>
2021-05-08 17:03:24 +0800

    (showdoc)增加新实例fy4air_sounding_hgt卫星观测

---
+++ metdig/onestep/diag_observation.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import numpy as np
+
+from metdig.io.cassandra import get_model_grid
+from metdig.io.cassandra import get_obs_stations
+from metdig.io.cassandra import get_fy_awx
+
+from metdig.onestep.lib.utility import get_map_area
+from metdig.onestep.lib.utility import mask_terrian
+from metdig.onestep.lib.utility import date_init
+
+from metdig.products.diag_observation import draw_fy4air_sounding_hgt
+
+import metdig.cal as mdgcal
+
+
+def fy4air_sounding_hgt(ir_obs_time=None, ir_channel=9,
+                        sounding_obs_time=None,
+                        hgt_init_time=None, hgt_fhour=24,
+                        is_mask_terrain=True,
+                        area='全国', is_return_data=False, is_draw=True, **products_kwargs):
+    ret = {}
+
+    # get area
+    map_extent = get_map_area(area)
+
+    # get data
+    ir = get_fy_awx(obs_time=ir_obs_time, data_name='fy4al1', var_name='tbb', channel=ir_channel, extent=map_extent)
+    hgt = get_model_grid(init_time=hgt_init_time, fhour=hgt_fhour, data_name='ecmwf', var_name='hgt', level=500, extent=map_extent)
+    sounding_wsp = get_obs_stations(obs_time=sounding_obs_time, data_name='plot_high', var_name='wsp', level=500, extent=map_extent)
+    sounding_wdir = get_obs_stations(obs_time=sounding_obs_time, data_name='plot_high', var_name='wdir', level=500, extent=map_extent)
+
+    # 计算uv
+    sounding_u, sounding_v = mdgcal.wind_components(sounding_wsp, sounding_wdir)
+    # 过滤超过100的uv
+    sounding_u.stda.where((sounding_u.stda.data < 100) & (sounding_v.stda.data < 100), np.nan)
+    sounding_v.stda.where((sounding_u.stda.data < 100) & (sounding_v.stda.data < 100), np.nan)
+
+    if is_return_data:
+        dataret = {'ir': ir, 'hgt': hgt, 'sounding_u': sounding_u, 'sounding_v': sounding_v}
+        ret.update({'data': dataret})
+
+    hgt = mdgcal.gaussian_filter(hgt, sigma=1, order=0)
+
+    # plot
+    if is_draw:
+        drawret = draw_fy4air_sounding_hgt(ir, hgt, sounding_u, sounding_v, map_extent=map_extent, **products_kwargs)
+        ret.update(drawret)
+
+    return ret
---
+++ metdig/products/diag_observation.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import os
+import datetime
+import numpy as np
+import pandas as pd
+
+import cartopy.crs as ccrs
+
+from metdig.graphics.barbs_method import *
+from metdig.graphics.contour_method import *
+from metdig.graphics.contourf_method import *
+from metdig.graphics.pcolormesh_method import *
+from metdig.graphics.draw_compose import *
+
+
+def draw_fy4air_sounding_hgt(ir, hgt, sounding_u, sounding_v, map_extent=(60, 145, 15, 55),
+                             ir_pcolormesh_kwargs={},  hgt_contour_kwargs={},
+                             **pallete_kwargs):
+
+    ir_time = ir.stda.time[0]
+    hgt_time = hgt.stda.fcst_time[0]
+    sounding_time = sounding_u.stda.time[0]
+    ir_channel = ir.stda.level # 卫星数据level代表通道号
+
+    if ir_channel == 9:
+        ir_name = '水汽图像'
+        ir_cmap = 'met/wv_enhancement'
+        uv_color = 'black'
+    elif ir_channel == 12:
+        ir_name = '红外(10.8微米)'
+        ir_cmap = 'met/ir_enhancement1'
+        uv_color = 'white'
+
+    title = 'FY4A{}观测 探空观测 高度场'.format(ir_name)
+    forcast_info = '卫星观测时间: {0:%Y}年{0:%m}月{0:%d}日{0:%H}时\n探空观测时间: {1:%Y}年{1:%m}月{1:%d}日{1:%H}时\n高度场时间: {2:%Y}年{2:%m}月{2:%d}日{2:%H}时\nwww.nmc.cn'.format(
+        ir_time, sounding_time, hgt_time)
+    png_name = '卫星观测{1}_{0:%Y}年{0:%m}月{0:%d}日{0:%H}时.png'.format(ir_time, ir_name)
+
+    obj = horizontal_compose(title=title, description=forcast_info, png_name=png_name, map_extent=map_extent, **pallete_kwargs)
+    ir_pcolormesh(obj.ax, ir, cmap=ir_cmap, kwargs=ir_pcolormesh_kwargs)
+    obj.ax.barbs(sounding_u.lon.values,
+                 sounding_u.lat.values,
+                 sounding_u.stda.data.values.squeeze() * 2.5,
+                 sounding_v.stda.data.values.squeeze() * 2.5,
+                 transform=ccrs.PlateCarree(), fill_empty=False,
+                 sizes=dict(emptybarb=0.0),
+                 zorder=2, color=uv_color, alpha=0.7, lw=1.5, length=7)
+    hgt_contour(obj.ax, hgt, kwargs=hgt_contour_kwargs)
+    return obj.save()
