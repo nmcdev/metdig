798135525d3de197e9ef50f5a08e0d603d11935a
heygy <heygy123@126.com>
2021-11-23 15:19:55 +0800

    （showdoc）增加阵风和平均风检验

--- metdig/products/veri_synop.py
+++ metdig/products/veri_synop.py
@@ -7,14 +7,70 @@ import pandas as pd
 
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import meteva.base as meb
 
 import datetime
+from metdig.graphics import barbs_method
+from metdig.graphics import contour_method
+from metdig.graphics import contourf_method
+from metdig.graphics import pcolormesh_method
+import metdig.utl.utl_stda_station as utl_stda_station
+from metdig.cal.other import wind_components
 
 from metdig.graphics.barbs_method import *
 from metdig.graphics.contour_method import *
 from metdig.graphics.contourf_method import *
 from metdig.graphics.pcolormesh_method import *
 from metdig.graphics.draw_compose import *
+import metdig.graphics.lib.utility as utl
+
+def draw_veri_gust10m(gust10m_fcst,gust10m_obs,gustdir10m_obs,
+                      map_extent=(60, 145, 15, 55),
+                      gust10m_barb_kwargs={},gust10m_pcolormesh_kwargs={},colorbar_kwargs={},
+                      **pallete_kwargs
+                      ):
+    # tmx24_2m_obs['level']=2
+    # tmx24_2m_fcst_sta=meb.interp_gs_linear(tmx24_2m_fcst,tmx24_2m_obs)
+    # tmx24_2m_obs_fcst=meb.combine_on_obTime_id(tmx24_2m_obs,tmx24_2m_fcst_sta)
+
+    init_time = pd.to_datetime(gust10m_fcst.coords['time'].values[0]).replace(tzinfo=None).to_pydatetime()
+    fhour = int(gust10m_fcst['dtime'].values[0])
+    fcstTime = init_time + datetime.timedelta(hours=fhour)
+    data_name = gust10m_fcst['member'].values[0]
+    # title = '[{}] 阵风天气预报检验'.format(data_name.upper())
+    title = '[{0:}] {1:}预报检验'.format(data_name.upper(),gust10m_obs.attrs['var_cn_name'])
+
+
+    forcast_info = '起报时间: {0:%Y}年{0:%m}月{0:%d}日{0:%H}时\n观测时间: {1:%Y}年{1:%m}月{1:%d}日{1:%H}时\n预报时效: {2}小时'.format(init_time, fcstTime, fhour)
+    png_name = '{2}_阵风天气_预报检验_分析时间_{0:%Y}年{0:%m}月{0:%d}日{0:%H}时_预报时效_{1:}小时.png'.format(fcstTime, fhour, data_name.upper())
+
+    
+    obj = horizontal_compose(title=title, description=forcast_info, png_name=png_name, map_extent=map_extent, kwargs=pallete_kwargs)
+    gust10m_obs=gust10m_obs.where(gust10m_obs['id'].isin(list(set(gust10m_obs['id']).intersection(gustdir10m_obs['id'])))).dropna().reset_index(drop=True)
+    gustdir10m_obs=gustdir10m_obs.where(gustdir10m_obs['id'].isin(list(set(gust10m_obs['id']).intersection(gustdir10m_obs['id'])))).dropna().reset_index(drop=True)
+    gust10m_fcst_sta=utl_stda_station.gridstda_to_stastda(gust10m_fcst,
+                    points={'id':gust10m_obs.stda.get_dim_value('id'),'lon': gust10m_obs.stda.get_dim_value('lon'), 'lat': gust10m_obs.stda.get_dim_value('lat')})
+    u_obs,v_obs=wind_components(gust10m_obs,gustdir10m_obs)
+    ####绘制模式预报阵风
+    pcolormesh_method.gust_pcolormesh(obj.ax,gust10m_fcst,alpha=0.5,colorbar_kwargs={'pos':'right','label':'预报风速 (m/s)'},kwargs=gust10m_pcolormesh_kwargs)
+
+    ####绘制误差
+    levels=np.arange(-10,10.5,0.5)
+    cmap, norm = cm_collected.get_cmap('ncl/ncl_default', extend='both', levels=levels)
+    for ilev,level in enumerate(levels[0:-1]):
+        cond=(((gust10m_fcst_sta.iloc[:,6]-gust10m_obs.iloc[:,6])>=levels[ilev]) & ((gust10m_fcst_sta.iloc[:,6]-gust10m_obs.iloc[:,6])<levels[ilev+1]))
+        barbs_2d(obj.ax, u_obs.where(cond).dropna(), v_obs.where(cond).dropna(), length=5, lw=0.5, sizes=dict(emptybarb=0.0), regrid_shape=None,
+                color=cmap.colors[ilev], kwargs=gust10m_barb_kwargs)
+    cond_min=((gust10m_fcst_sta.iloc[:,6]-gust10m_obs.iloc[:,6])>=levels[-1])
+    barbs_2d(obj.ax, u_obs.where(cond_min).dropna(), v_obs.where(cond_min).dropna(), length=5, lw=0.5, sizes=dict(emptybarb=0.0), regrid_shape=None,
+        color=cmap.colors[-1],kwargs=gust10m_barb_kwargs)
+    cond_max=((gust10m_fcst_sta.iloc[:,6]-gust10m_obs.iloc[:,6])<levels[0])
+    barbs_2d(obj.ax, u_obs.where(cond_max).dropna(), v_obs.where(cond_max).dropna(), length=5, lw=0.5, sizes=dict(emptybarb=0.0), regrid_shape=None,
+                color=cmap.colors[0], kwargs=gust10m_barb_kwargs)
+    utl.add_colorbar(obj.ax,mpl.cm.ScalarMappable(norm=norm,cmap=cmap),extend='both',label='deviation (m/s)', kwargs=colorbar_kwargs)
+
+    return obj.save()
 
 def draw_veri_heatwave(tmx24_2m_fcst, tmx24_2m_obs,
                       map_extent=(60, 145, 15, 55),
