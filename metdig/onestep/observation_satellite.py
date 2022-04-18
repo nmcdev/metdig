0db93a3f8a48377a27a09ef00c7968475a342538
heygy <heygy123@126.com>
2022-04-18 22:30:27 +0800

    （showdoc）添加卫星实况分析实例

--- metdig/onestep/observation_satellite.py
+++ metdig/onestep/observation_satellite.py
@@ -22,6 +22,98 @@ __all__ = [
     'fy4air_sounding_hgt',
 ]
 
+def fy4a_c009_hgt_uv_prmsl(ir_obs_time=None,
+                        init_time=None,fhour=12,data_name='cma_gfs',data_source='cmadaas',
+                        hgt_lev=500,uv_lev=850,
+                        is_mask_terrain=True,
+                        area=[70,140,15,55], is_return_data=False, is_draw=True, **products_kwargs):
+    ret = {}
+
+    # get area
+    map_extent = get_map_area(area)
+
+    # get data
+    ir = get_fy_awx(obs_time=ir_obs_time, data_name='fy4al1', var_name='tbb', channel=9, extent=map_extent)
+    hgt = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='hgt', level=hgt_lev, extent=map_extent)
+    u = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='u', level=uv_lev, extent=map_extent)
+    v = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='v', level=uv_lev, extent=map_extent)
+    prmsl = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='prmsl', extent=map_extent)
+
+    hgt = mdgcal.gaussian_filter(hgt, sigma=1, order=0)
+    prmsl = mdgcal.gaussian_filter(prmsl, sigma=2, order=0)
+
+    # 隐藏被地形遮挡地区
+    if is_mask_terrain:
+        psfc = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='psfc', extent=map_extent)
+        hgt = mask_terrian(psfc, hgt)
+        u = mask_terrian(psfc, u)
+        v = mask_terrian(psfc, v)
+
+    if is_return_data:
+        dataret = {'ir': ir, 'hgt': hgt, 'u': u, 'v': v, 'prmsl': prmsl}
+        ret.update({'data': dataret})
+
+    # plot
+    if is_draw:
+
+        drawret = draw_obssate.draw_fy4a_c009_hgt_uv_prmsl(ir, hgt, u, v, prmsl, map_extent=map_extent,**products_kwargs)
+        ret.update(drawret)
+
+    if ret:
+        return ret
+
+if __name__=='__main__':
+    import matplotlib.pyplot as plt
+    fy4a_c009_hgt_uv_prmsl(ir_obs_time=datetime.datetime(2022,4,12,8),
+        init_time=datetime.datetime(2022,4,12,8),fhour=3,add_city=False)
+    plt.show()
+
+def fy4a_c012_hgt_uv_prmsl(ir_obs_time=None,
+                        init_time=None,fhour=12,data_name='cma_gfs',data_source='cmadaas',
+                        hgt_lev=500,uv_lev=850,
+                        is_mask_terrain=True,
+                        area=[70,140,15,55], is_return_data=False, is_draw=True, **products_kwargs):
+    ret = {}
+
+    # get area
+    map_extent = get_map_area(area)
+
+    # get data
+    ir = get_fy_awx(obs_time=ir_obs_time, data_name='fy4al1', var_name='tbb', channel=12, extent=map_extent)
+    hgt = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='hgt', level=hgt_lev, extent=map_extent)
+    u = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='u', level=uv_lev, extent=map_extent)
+    v = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='v', level=uv_lev, extent=map_extent)
+    prmsl = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='prmsl', extent=map_extent)
+
+    hgt = mdgcal.gaussian_filter(hgt, sigma=1, order=0)
+    prmsl = mdgcal.gaussian_filter(prmsl, sigma=2, order=0)
+
+    # 隐藏被地形遮挡地区
+    if is_mask_terrain:
+        psfc = get_model_grid(init_time=init_time, fhour=fhour, data_name=data_name, var_name='psfc', extent=map_extent)
+        hgt = mask_terrian(psfc, hgt)
+        u = mask_terrian(psfc, u)
+        v = mask_terrian(psfc, v)
+
+    if is_return_data:
+        dataret = {'ir': ir, 'hgt': hgt, 'u': u, 'v': v, 'prmsl': prmsl}
+        ret.update({'data': dataret})
+
+    # plot
+    if is_draw:
+
+        drawret = draw_obssate.draw_fy4a_c012_hgt_uv_prmsl(ir, hgt, u, v, prmsl, map_extent=map_extent,**products_kwargs)
+        ret.update(drawret)
+
+    if ret:
+        return ret
+
+if __name__=='__main__':
+    import matplotlib.pyplot as plt
+    fy4a_c012_hgt_uv_prmsl(ir_obs_time=datetime.datetime(2022,4,12,8),
+        init_time=datetime.datetime(2022,4,12,8),fhour=3,add_city=False)
+    plt.show()
+
 def fy4a_c012_hgt_uv_cape(ir_obs_time=None,
                         init_time=None,fhour=12,data_name='cma_gfs',data_source='cmadaas',
                         hgt_lev=500,uv_lev=850,
@@ -50,7 +142,7 @@ def fy4a_c012_hgt_uv_cape(ir_obs_time=None,
         v = mask_terrian(psfc, v)
 
     if is_return_data:
-        dataret = {'ir': ir, 'hgt': hgt, 'u': u, 'v': v, 'pv': pv}
+        dataret = {'ir': ir, 'hgt': hgt, 'u': u, 'v': v, 'cape': cape}
         ret.update({'data': dataret})
 
     # plot
@@ -61,13 +153,6 @@ def fy4a_c012_hgt_uv_cape(ir_obs_time=None,
 
     if ret:
         return ret
-
-if __name__=='__main__':
-    import matplotlib.pyplot as plt
-    fy4a_c012_hgt_uv_cape(ir_obs_time=datetime.datetime(2022,4,1,8),
-        init_time=datetime.datetime(2022,4,1,8),fhour=3)
-    plt.show()
-
 
 # def fy4a_c012_hgt_uv_pv(ir_obs_time=None,
 #                         init_time=None,fhour=12,data_name='cma_gfs',data_source='cmadaas',
