611b76f2355e26379c254edcc73b3f1c59b7ab65
heygy <heygy123@126.com>
2022-06-02 16:39:13 +0800

    添加了自动拆分分别插值功能，优化速度

--- metdig/cal/interpolate.py
+++ metdig/cal/interpolate.py
@@ -6,15 +6,17 @@ from scipy.interpolate import LinearNDInterpolator
 import metdig.utl as mdgstda
 from metdig.io.lib import utility as utl
 from metdig.onestep.lib.utility import mask_terrian
-
+import math
 __all__ = [
     'interpolate_3d',
+    'interpolate_3d_whole_area'
 ]
 
-def interpolate_3d(stda, hgt, points, stda_sfc=None, psfc=None):
+def interpolate_3d(stda, hgt, points, stda_sfc=None, psfc=None,if_split=None):
     '''
 
-    [利用位势高度，站点高度，和各层模式数据进行三维不规则点插值，获取初步订正的山地地形站点预报]
+    [利用位势高度，站点高度，和各层模式数据进行三维不规则点插值，获取初步订正的山地地形站点预报，
+    对输入点的范围进行自动切割循环，以控制输入三维插值方法的数据范围，达到加速的效果]
 
     Arguments:
         stda {[stda]} -- [被插值的要素]
@@ -22,8 +24,49 @@ def interpolate_3d(stda, hgt, points, stda_sfc=None, psfc=None):
         points {[{'lon':[110],'lat':[30],'alt':[1000]}]} -- [{被插值站点的经度，维度，高度}]
         stda_sfc {[stda]} -- [可选量,stda的对应地面量，当站点高度低于hgt中对应位置高度最小值，则直接用stda_sfc中的线性插值结果]
         psfc {[stda]} -- [当psfc<stda的等压面，则为模式地下部分，赋值nan，如果此时stda_sfc不为None,则为其线性插值结果]
-
+        if_split  {None or int} -- [是否采用站点数据范围拆分方法加速多维插值]
     Returns:
+        [stda] -- [被插值后的站点数据,超出给定范围复制nan]
+    '''
+    pnt_extent=[math.floor(min(points['lon'])),math.ceil(max(points['lon'])),math.floor(min(points['lat'])),math.ceil(max(points['lat']))]
+
+    if if_split is None:
+        stda_sta=interpolate_3d_whole_area(stda=stda,hgt=hgt,points=points,stda_sfc=stda_sfc,psfc=psfc)
+    else:
+        sta_lons=np.array(points['lon'])
+        sta_lats=np.array(points['lat'])
+        sta_alts=np.array(points['lat'])
+        sta_ids=np.array(points['id'])
+        stda_sta=[]
+        lon_s=pnt_extent[0]
+        while(lon_s < pnt_extent[1]+if_split):
+            lon_e=lon_s+if_split
+            lat_s=pnt_extent[2]
+            while(lat_s < pnt_extent[3]+if_split):
+                lat_e=lat_s+if_split
+                idx_in=np.where((sta_lons>=lon_s) & (sta_lats>=lat_s) & (sta_lons<lon_e) & (sta_lats<lat_e))
+                if(idx_in[0].size > 0):
+                    points_split={'lon':sta_lons[idx_in],'lat':sta_lats[idx_in],'alt':sta_alts[idx_in],'id':sta_ids[idx_in]}
+                    stda_cut=utl.area_cut(stda,extent=[lon_s,lon_e,lat_s,lat_e])
+                    hgt_cut=utl.area_cut(hgt,extent=[lon_s,lon_e,lat_s,lat_e])
+                    stda_sta.append(interpolate_3d_whole_area(stda=stda_cut,hgt=hgt_cut,points=points_split,stda_sfc=stda_sfc,psfc=psfc))
+                lat_s=lat_e
+            lon_s=lon_e
+        stda_sta=pd.concat(stda_sta)
+    return stda_sta
+
+def interpolate_3d_whole_area(stda, hgt, points, stda_sfc=None, psfc=None):
+    '''
+
+    [利用位势高度，站点高度，和各层模式数据进行三维不规则点插值，获取初步订正的山地地形站点预报,该方法对全区域进行直接三维插值，对于范围较大的数据运行效率会很慢]
+
+    Arguments:
+        stda {[stda]} -- [被插值的要素]
+        hgt {[stda]} -- [模式位势高度]
+        points {'lon':[110],'lat':[30],'alt':[1000]} -- [{被插值站点的经度，维度，高度}]
+        stda_sfc {[stda]} -- [可选量,stda的对应地面量，当站点高度低于hgt中对应位置高度最小值，则直接用stda_sfc中的线性插值结果]
+        psfc {[stda]} -- [当psfc<stda的等压面，则为模式地下部分，赋值nan，如果此时stda_sfc不为None,则为其线性插值结果]
+    Returns:    
         [stda] -- [被插值后的站点数据,超出给定范围复制nan]
     '''
     stda_sta=[]
@@ -104,20 +147,14 @@ if __name__ == '__main__':
     import numpy as np
     import pandas as pd
     id_selected=54511
-    obs_time=datetime(2022,4,27,20)
-    init_time=datetime(2022,4,27,8)
+    init_time=datetime(2022,5,31,8)
     levels=[1000,850,700,500,200,100]
     fhour=36
-    data_source='cmadaas'
-    data_name='ecmwf'
+    data_source='cassandra'
+    data_name='ecmwf_ens'
 
-    hgt_sounding=metdig.io.cassandra.get_tlogp(obs_time=obs_time,data_name='tlogp',var_name='hgt',id_selected=id_selected)
-    tmp_sounding=metdig.io.cassandra.get_tlogp(obs_time=obs_time,data_name='tlogp',var_name='tmp',id_selected=id_selected)
-    wsp_sounding=metdig.io.cassandra.get_tlogp(obs_time=obs_time,data_name='tlogp',var_name='wsp',id_selected=id_selected)
-    wdir_sounding=metdig.io.cassandra.get_tlogp(obs_time=obs_time,data_name='tlogp',var_name='wdir',id_selected=id_selected)
-    u_sounding,v_sounding=metdig.cal.other.wind_components(wsp_sounding,wdir_sounding)
-
-    points={'lon':list(u_sounding.stda.lon),'lat':list(u_sounding.stda.lat),'alt':hgt_sounding.stda.values.tolist(),'id':list(hgt_sounding.stda.id)}
+    sta_info=pd.read_csv(r'L:\RoutineJob\FISU\obs/sta_info_national.csv',encoding='gbk')
+    points={'lon':sta_info['lon'].to_list(),'lat':sta_info['lat'].to_list(),'alt':sta_info['alt'].to_list(),'id':sta_info['id'].to_list()}
 
     # get data
     extent=[min(points['lon'])-1,max(points['lon'])+1,min(points['lat'])-1,max(points['lat'])+1]
@@ -137,6 +174,9 @@ if __name__ == '__main__':
     hgt=metdig.io.get_model_3D_grid(init_time=init_time,fhour=fhour,levels=levels,
                                 data_source=data_source,data_name=data_name,var_name='hgt',extent=extent)
 
-    u10m_md=metdig.cal.interpolate.interpolate_3d(u, hgt, points, stda_sfc=u10m)
-    v10m_md=metdig.cal.interpolate.interpolate_3d(v, hgt, points, stda_sfc=v10m)
-    t2m_md=metdig.cal.interpolate.interpolate_3d(tmp, hgt, points, stda_sfc=t2m)
+    s=datetime.now()
+    u10m_md=metdig.cal.interpolate.interpolate_3d(u, hgt, points, stda_sfc=u10m,if_split=None)
+    e=datetime.now()
+    print((e-s).total_seconds())
+    # v10m_md=metdig.cal.interpolate.interpolate_3d(v, hgt, points, stda_sfc=v10m)
+    # t2m_md=metdig.cal.interpolate.interpolate_3d(tmp, hgt, points, stda_sfc=t2m)
