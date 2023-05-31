31f48ab08df3413e7861d88edf44872a1b864b58
heygy <heygy123@126.com>
2023-05-31 17:14:37 +0800

    优化

--- metdig/products/diag_qpf.py
+++ metdig/products/diag_qpf.py
@@ -70,8 +70,9 @@ def draw_hgt_rain(hgt, rain, map_extent=(60, 145, 15, 55),
                   hgt_contour_kwargs={},rain_pcolormesh_kwargs={},
                   **pallete_kwargs):
     init_time = pd.to_datetime(rain.coords['time'].values[0]).replace(tzinfo=None).to_pydatetime()
-    fhour = int(rain['dtime'].values[0])
-    fcst_time = init_time + datetime.timedelta(hours=fhour)
+    fhour_rain = int(rain['dtime'].values[0])
+    fhour_hgt = int(rain['dtime'].values[0])
+    fcst_time = init_time + datetime.timedelta(hours=fhour_rain)
     level = hgt['level'].values[0]
 
     valid_time = rain.attrs['valid_time']
@@ -80,8 +81,8 @@ def draw_hgt_rain(hgt, rain, map_extent=(60, 145, 15, 55),
     title = '[{}] {}hPa 位势高度场，{}'.format(data_name.upper(), level, var_cn_name)
 
     forcast_info = '起报时间: {0:%Y}年{0:%m}月{0:%d}日{0:%H}时\n预报时间: {1:%Y}年{1:%m}月{1:%d}日{1:%H}时\n预报时效: {2}小时(降水{3}小时)'.format(
-        init_time, fcst_time, fhour, fhour + 12)
-    png_name = '{2}_高度场_{3}_预报_起报时间_{0:%Y}年{0:%m}月{0:%d}日{0:%H}时预报时效_{1:}小时.png'.format(init_time, fhour, data_name.upper(), var_cn_name)
+        init_time, fcst_time, fhour_hgt, fhour_rain)
+    png_name = '{2}_高度场_{3}_预报_起报时间_{0:%Y}年{0:%m}月{0:%d}日{0:%H}时预报时效_{1:}小时.png'.format(init_time, fhour_rain, data_name.upper(), var_cn_name)
 
     obj = horizontal_compose(title=title, description=forcast_info, png_name=png_name, map_extent=map_extent, kwargs=pallete_kwargs)
     hgt_contour(obj.ax, hgt, kwargs=hgt_contour_kwargs)
